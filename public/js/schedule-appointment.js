7215b46511e96294c0bdd1bf57ef44c35520e522
Christopher Backes <Christopher.Backes@gmail.com>
2022-03-07 22:24:38 -0500

    refactoring/adding schedule to owner dash

---
+++ public/js/schedule-appointment.js
@@ -0,0 +1,22 @@
+async function addDogtoCalendar(event) {
+  event.preventDefault();
+
+  const calendarId = this.parentNode.dataset.calendar;
+
+  const response = await fetch(`/api/calendars/${calendarId}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+      dog_id: null,
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  if (response.ok) {
+    document.location.reload();
+  }
+}
+
+document
+  .querySelectorAll('.schedule-dog')
+  .forEach((element) => element.addEventListener('click', addDogtoCalendar));
