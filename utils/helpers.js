006b276c84418c0fbe4ba26cbd1bc8401b2891a5
Daniel Kim <daniel.d.kim13@gmail.com>
2022-03-09 15:35:46 -0500

    walker dashboard somewhat refined

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,7 +1,7 @@
 module.exports = {
-  owner_profile_pic: (data) => {
+  profile_pic: (data) => {
     if (data.profile_pic !== 'null') {
-      return `<img src="${data.profile_pic}" alt="owner profile image">`;
+      return `<img src="${data.profile_pic}" alt="profile image">`;
     } else {
       return `<img src="https://st.depositphotos.com/2218212/2938/i/950/depositphotos_29387653-stock-photo-facebook-profile.jpg" alt="stock profile image">`;
     }
