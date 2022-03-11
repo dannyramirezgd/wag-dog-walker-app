c89de2d574d54cf75b641350d7112b381be40586
Danny Ramirez <danny.ramirezgd@gmail.com>
2022-03-11 08:33:47 -0500

    added some environment variable protection

--- controllers/homeRoutes/contact-us-route.js
+++ controllers/homeRoutes/contact-us-route.js
@@ -1,22 +1,21 @@
 const router = require('express').Router();
 const nodemailer = require('nodemailer');
+require('dotenv').config();
 
 // current connected to free email testing application since didn't want to route to my personal email.
 let transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 587,
   auth: {
-    user: 'team.wag.2022@gmail.com',
-    pass: 'P@ssword1!',
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD,
   },
 });
 
 // email sending route.
 router.post('/send', (req, res) => {
-  console.log(req.body);
-
   const mailData = {
-    from: 'team.wag.2022@gmail.com',
+    from: process.env.EMAIL,
     to: 'danny.ramirezgd@gmail.com',
     subject: req.body.subject + ' ' + 'from' + ' ' + req.body.name,
     text: req.body.message + 'POC email@' + ' ' + req.body.email,
